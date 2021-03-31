1ab9c29b491024251becb535675d7b839aa1dacc
Mehmet Buyukertas <acizkul6327@gmail.com>
2021-03-31 12:39:25 +0200

    Create new class test to JUnit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,22 @@
+
+
+
+
+
 public class Calculator {
+
+   //Calculator uzerine gelip sag tikla
+    //Generate goreceksin ona tikla
+    //Sonra Test' e tikla
+    //Sonra fix olacak ona tikliyorsun
+    //Sana otomatik Junit POM dependenciesleri veriyor
+    //Ayrica Mainde yaptigimiz bu siniftaki olaydan dolayi
+    //Test package icinde de otomatik sinif olusturuluyor.
+
+
+    public static int add(int num1,int num2) {
+
+        return num1+num2;
+
+    }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+  @Test
+  void testCase1(){
+
+  }
+
+
+    @Test
+    void testCase2(){
+
+
+    }
+}
