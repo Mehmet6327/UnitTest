bf3e5d41d3ad4f1c668b054d432627f7fbc6a4c3
Mehmet Buyukertas <acizkul6327@gmail.com>
2021-03-31 15:50:59 +0200

    added new tests

--- src/test/java/CheckingAccountTest.java
+++ src/test/java/CheckingAccountTest.java
@@ -1,28 +1,55 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckingAccountTest {
 
+    CheckingAccount checkingAccount;
+    private static  int count;
+
+    @BeforeAll
+    static void beforeAll(){
+        count++;
+    }
+
     @BeforeEach
     void setUp() {
+        checkingAccount = new CheckingAccount();
+        checkingAccount.setInfo(100,123457L,"Mehmet");
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println("count++ = " + count++);
+    }
+    @AfterAll
+    static void afterAll(){
+        System.out.println("This executes after any test cases.Count:" + count++);
     }
 
     @Test
     void deposit() {
+
+
+        assertEquals(200,checkingAccount.deposit(100));
     }
 
     @Test
     void withdraw() {
+
+
+
+        assertEquals(80,checkingAccount.withdraw(20));
+
+
     }
+
 
     @Test
     void purchase() {
+
+
+
+        assertEquals(-65,checkingAccount.purchase("Shoes",130));
     }
 }
